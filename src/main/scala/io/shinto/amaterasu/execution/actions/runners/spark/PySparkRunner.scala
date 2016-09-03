7e59d7e7ac203bf950d20daf923018cbac97528c
Yaniv Rodenski <roadan@gmail.com>
2016-09-03 23:33:50 +1000

    mid work

---
+++ src/main/scala/io/shinto/amaterasu/execution/actions/runners/spark/PySparkRunner.scala
@@ -0,0 +1,10 @@
+package io.shinto.amaterasu.execution.actions.runners.spark
+
+import io.shinto.amaterasu.Logging
+
+/**
+  * Created by roadan on 9/2/16.
+  */
+class PySparkRunner extends Logging {
+
+}
---
+++ src/test/scala/io/shinto/amaterasu/spark/PySparkRunnerTests.scala
@@ -0,0 +1,8 @@
+package io.shinto.amaterasu.spark
+
+/**
+  * Created by roadan on 9/2/16.
+  */
+class PySparkRunnerTests {
+
+}
