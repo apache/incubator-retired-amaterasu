c4e752edba467dd90073c2e0fd7bc16295a04264
Yaniv Rodenski <roadan@gmail.com>
2017-04-23 11:13:10 +1000

    fixed path in python tests

--- executor/src/test/resources/simple-python-err.py
+++ executor/src/test/resources/simple-python-err.py
@@ -1,5 +1,5 @@
 data = [1, 2, 3, 4, 5]
 1/0
 
-with open('/Users/roadan/in.txt', 'a') as the_file:
+with open('/tmp/amatest-in.txt', 'a') as the_file:
     the_file.write('hi there\n') # python will convert \n to os.linesep
