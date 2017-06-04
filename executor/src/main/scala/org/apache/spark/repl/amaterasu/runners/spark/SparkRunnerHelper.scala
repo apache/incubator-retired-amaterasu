94877ae943129be895b8a7fd6871d9218be7b300
Eyal Ben Ivri <eyalbenivri@gmail.com>
2017-06-04 17:20:11 +0200

    - aligned jre version to 1.8 in all projects - aligned scala version to 2.11.11 - got rid of SQLContext in SparkSqlRunner - endless attempts (and covfefe attempts) to reconcile ClassCastException     I believe this is due to spark 2 changes in the interpreter implementations     see: https://issues.apache.org/jira/browse/SPARK-20525

--- executor/src/main/scala/org/apache/spark/repl/amaterasu/runners/spark/SparkRunnerHelper.scala
+++ executor/src/main/scala/org/apache/spark/repl/amaterasu/runners/spark/SparkRunnerHelper.scala
@@ -2,7 +2,7 @@ package org.apache.spark.repl.amaterasu.runners.spark
 
 import io.shinto.amaterasu.common.configuration.ClusterConfig
 import io.shinto.amaterasu.common.runtime.Environment
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.util.Utils
 
@@ -25,6 +25,8 @@ object SparkRunnerHelper {
     Thread.currentThread().setContextClassLoader(getClass.getClassLoader)
 
     val conf = new SparkConf(true)
+      .setAppName(sparkAppName)
+      .set("spark.master", env.master)
       .set("spark.executor.uri", s"http://$getNode:${config.Webserver.Port}/spark-2.1.0-1-bin-2.7.tgz")
       .set("spark.driver.host", "192.168.33.11")
       .set("spark.submit.deployMode", "client")
@@ -37,7 +39,8 @@ object SparkRunnerHelper {
       //.set("spark.hadoop.validateOutputSpecs", "false")
 
       .setJars(jars)
-
+    // we need SparkContext instance to be alive in order for the application web ui to be available.
+    val sc = new SparkContext(conf)
     val rootDir = conf.getOption("spark.repl.classdir").getOrElse(Utils.getLocalDir(conf))
     val outputDir = Utils.createTempDir(root = rootDir, namePrefix = "repl")
 
@@ -46,7 +49,6 @@ object SparkRunnerHelper {
     val sparkSession = SparkSession.builder
       .appName(sparkAppName)
       .master(env.master)
-
       .enableHiveSupport()
       .config(conf).getOrCreate()
 
