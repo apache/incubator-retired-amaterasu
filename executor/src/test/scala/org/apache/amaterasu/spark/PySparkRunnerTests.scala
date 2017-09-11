9a835c0c0a9ea088ede182a7e4c227f7b74b3adf
Yaniv Rodenski <roadan@gmail.com>
2017-09-11 18:55:23 +1000

    fixed double instanciation of tests

--- executor/src/test/scala/org/apache/amaterasu/spark/PySparkRunnerTests.scala
+++ executor/src/test/scala/org/apache/amaterasu/spark/PySparkRunnerTests.scala
@@ -9,11 +9,12 @@ import org.apache.amaterasu.utilities.TestNotifier
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.repl.amaterasu.runners.spark.SparkRunnerHelper
 import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, FlatSpec, Matchers}
 
 import scala.collection.JavaConverters._
 import scala.io.Source
 
+@DoNotDiscover
 class PySparkRunnerTests extends FlatSpec with Matchers with BeforeAndAfterAll {
 
   Logger.getLogger("org").setLevel(Level.OFF)
@@ -22,7 +23,7 @@ class PySparkRunnerTests extends FlatSpec with Matchers with BeforeAndAfterAll {
   Logger.getLogger("jetty").setLevel(Level.OFF)
   Logger.getRootLogger.setLevel(Level.OFF)
 
-  var sc: SparkContext = _
+  //var sc: SparkContext = _
   var factory: ProvidersFactory = _
 
   def delete(file: File) {
@@ -32,12 +33,12 @@ class PySparkRunnerTests extends FlatSpec with Matchers with BeforeAndAfterAll {
   }
 
   override protected def beforeAll(): Unit = {
-    val env = new Environment()
-    val notifier = new TestNotifier()
+//    val env = new Environment()
+//    val notifier = new TestNotifier()
 
     // this is an ugly hack, getClass.getResource("/").getPath should have worked but
     // stopped working when we moved to gradle :(
-    val resources = new File(getClass.getResource("/spark_intp.py").getPath).getParent
+//    val resources = new File(getClass.getResource("/spark_intp.py").getPath).getParent
 //    val resourceDir = new File(new File(getClass.getResource("/spark_intp.py").getPath).getParent)
 
 
@@ -47,25 +48,25 @@ class PySparkRunnerTests extends FlatSpec with Matchers with BeforeAndAfterAll {
 //      .set("spark.local.ip", "127.0.0.1")
 //      .set("spark.ui.port", "4081")
 //      .setExecutorEnv("PYTHONPATH", resources)
-    val conf = Map[String, Any](
-      "spark.cassandra.connection.host" -> "127.0.0.1",
-      "sourceTable" -> "documents",
-      "spark.local.ip" -> "127.0.0.1"
-    )
-    env.master = "local[1]"
-    if (env.configuration != null) env.configuration ++ "pysparkPath" -> "/usr/bin/python" else env.configuration = Map(
-      "pysparkPath" -> "/usr/bin/python",
-      "cwd" -> resources
-    )
-    val excEnv = Map[String, Any](
-      "PYTHONPATH" -> resources
-    )
+//    val conf = Map[String, Any](
+//      "spark.cassandra.connection.host" -> "127.0.0.1",
+//      "sourceTable" -> "documents",
+//      "spark.local.ip" -> "127.0.0.1"
+//    )
+//    env.master = "local[1]"
+//    if (env.configuration != null) env.configuration ++ "pysparkPath" -> "/usr/bin/python" else env.configuration = Map(
+//      "pysparkPath" -> "/usr/bin/python",
+//      "cwd" -> resources
+//    )
+//    val excEnv = Map[String, Any](
+//      "PYTHONPATH" -> resources
+//    )
 
     super.beforeAll()
   }
 
   override protected def afterAll(): Unit = {
-    sc.stop()
+    //sc.stop()
     val pysparkDir = new File(getClass.getResource("/pyspark").getPath)
     val py4jDir = new File(getClass.getResource("/py4j").getPath)
     delete(pysparkDir)
