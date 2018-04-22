5b4e8eb2bd564af37cbcda929720d75323bb1cfb
Yaniv Rodenski <roadan@gmail.com>
2018-04-22 18:56:40 +1000

    fixed merge issue

--- leader/src/main/scripts/ama-start-yarn.sh
+++ leader/src/main/scripts/ama-start-yarn.sh
@@ -86,7 +86,7 @@ done
 echo "repo: ${REPO} "
 echo "force-bin: ${FORCE_BIN}"
 export HADOOP_USER_CLASSPATH_FIRST=true
-CMD="yarn jar ${BASEDIR}/bin/leader-0.2.0-incubating-all.jar org.apache.amaterasu.leader.yarn.Client --home ${BASEDIR}"
+CMD="yarn jar ${BASEDIR}/bin/leader-0.2.0-incubating-rc2-all.jar org.apache.amaterasu.leader.yarn.Client --home ${BASEDIR}"
 
 if [ -n "$REPO" ]; then
     echo "repo is ${REPO}"
