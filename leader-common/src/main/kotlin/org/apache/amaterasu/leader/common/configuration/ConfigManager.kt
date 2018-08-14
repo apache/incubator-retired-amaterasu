e94c33a017b064e9d1c7be365319ab1dbaf98fd2
Yaniv Rodenski <roadan@gmail.com>
2018-08-14 14:23:23 +1000

    added support for custom path in actions

--- leader-common/src/main/kotlin/org/apache/amaterasu/leader/common/configuration/ConfigManager.kt
+++ leader-common/src/main/kotlin/org/apache/amaterasu/leader/common/configuration/ConfigManager.kt
@@ -1,25 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.amaterasu.leader.common.configuration
 
 import com.uchuhimo.konf.Config
 import java.io.File
 
-class ConfigManager(env: String, repoPath: String) {
+class ConfigManager(private val env: String, private val repoPath: String) {
 
-    private val envFolder = "$repoPath/$env"
-    val config = Config{
+    private val envFolder = "$repoPath/env/$env"
+
+    // this is currently public for testing reasons, need to reconsider
+    var config: Config = Config {}
+    private var baseconfig = Config {
         addSpec(Job)
     }
 
     init {
-        println("++++++++++++++++++++++++++++")
-        println(envFolder)
-        println("++++++++++++++++++++++++++++")
-        for(file in File(envFolder).listFiles()){
-            config.from.yaml.file(file)
+        for (file in File(envFolder).listFiles()) {
+            config = baseconfig.from.yaml.watchFile(file)
         }
     }
 
-//    fun getConfigurationWriter():  = {
-//        config.toYaml.toWrite
-//    }
+    fun getActionConfiguration(action: String, path: String = ""): Config {
+
+        var actionPath = "$repoPath/src/$action/env/$env"
+        if (!path.isEmpty()) {
+            actionPath = "$repoPath/$path"
+                    .replace("{env}", env)
+                    .replace("{action_name}", action)
+        }
+
+        var result = Config()
+
+        for (file in File(actionPath).listFiles()) {
+            result = config.from.yaml.file(file)
+        }
+        return result
+    }
 }
--- leader-common/src/test/kotlin/org/apache/amaterasu/leader/common/configuration/ConfigManagerTests.kt
+++ leader-common/src/test/kotlin/org/apache/amaterasu/leader/common/configuration/ConfigManagerTests.kt
@@ -1,16 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.amaterasu.leader.common.configuration
 
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.xcontext
+import org.jetbrains.spek.api.dsl.*
 import java.io.File
 
 class ConfigManagerTests : Spek({
 
     describe("creating a ConfigManager for a job with ") {
 
-        val marker = javaClass.getResource("marker.yml").path
+        val marker = this.javaClass.getResource("/maki.yml").path
         val repoPath = "${File(marker).parent}/test_repo"
         val cfg = ConfigManager("test", repoPath)
 
@@ -18,9 +32,18 @@ class ConfigManagerTests : Spek({
             assert(cfg.config[Job.master] == "yarn")
         }
 
-        xcontext("when an action is loaded") {
-            it("loads the specific configuration for the action"){
-                assert(cfg.config[Job.master] == "mesos")
+        on("getting an env for an action with default path") {
+            val startConf = cfg.getActionConfiguration("start")
+            it("loads the specific configuration defined in the actions folder"){
+                assert(startConf[Job.master] == "mesos")
+            }
+        }
+
+        on("getting an env for an action with a conf: property in the maki.yml"){
+            val step2conf = cfg.getActionConfiguration("step2", "src/{action_name}/{env}/")
+
+            it("loads the specific configuration defined in the actions folder"){
+                assert(step2conf[Job.name] == "step2")
             }
         }
     }
