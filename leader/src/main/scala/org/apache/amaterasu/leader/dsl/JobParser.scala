20ac890d7c7ccdd7dea84e129de6f26a28ca7802
guy.peleg <guy.peleg@myob.com>
2018-10-31 14:08:28 +1100

    seprate import statment while scala and kotlin mixed togther

--- leader/src/main/scala/org/apache/amaterasu/leader/dsl/JobParser.scala
+++ leader/src/main/scala/org/apache/amaterasu/leader/dsl/JobParser.scala
@@ -23,7 +23,8 @@ import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.apache.amaterasu.common.dataobjects.ActionData
 import org.apache.amaterasu.leader.execution.JobManager
-import org.apache.amaterasu.leader.execution.actions.{Action, ErrorAction, SequentialAction}
+import org.apache.amaterasu.leader.execution.actions.{ErrorAction, SequentialAction}
+import org.apache.amaterasu.leader.execution.actions.Action
 import org.apache.curator.framework.CuratorFramework
 
 import scala.collection.JavaConverters._
