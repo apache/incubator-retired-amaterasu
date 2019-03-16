ab4b636434e7b9c75e528c32b74ec7fc1a5fc7cc
Nadav Har Tzvi <nadavha@apache.org>
2019-03-16 15:46:35 +0200

    trying to get worker to use the correct python executable

--- frameworks/spark/pyspark_runtime/amaterasu_pyspark/runtime.py
+++ frameworks/spark/pyspark_runtime/amaterasu_pyspark/runtime.py
@@ -23,15 +23,15 @@ from amaterasu.runtime import AmaContextBuilder
 from .datasets import DatasetManager
 import os
 
-# spark_env_content = '''
-# #!/usr/bin/env bash
-#
-# export PYSPARK_PYTHON={}
-# '''.format(os.getenv("PYSPARK_PYTHON"))
-# pyspark_env_path = '{}/conf/spark_env.sh'.format(os.getenv('_'))
-#
-# with open(pyspark_env_path, 'w') as f:
-#     f.write(spark_env_content)
+spark_env_content = '''
+#!/usr/bin/env bash
+
+export PYSPARK_PYTHON={}
+'''.format(os.getenv("PYSPARK_PYTHON"))
+pyspark_env_path = '{}/conf/spark_env.sh'.format(os.getenv('_'))
+
+with open(pyspark_env_path, 'w') as f:
+    f.write(spark_env_content)
 
 
 class SparkAmaContextBuilder(AmaContextBuilder):
@@ -41,8 +41,7 @@ class SparkAmaContextBuilder(AmaContextBuilder):
         if self.ama_conf:
             self.spark_conf = SparkConf()\
                 .setAppName('amaterasu-{}-{}'.format(self.ama_conf.runtime.jobId, self.ama_conf.runtime.actionName))\
-                .setMaster(self.ama_conf.env.master)\
-                .set("spark.pyspark.python", os.getenv("_"))
+                .setMaster(self.ama_conf.env.master)
         else:
             self.spark_conf = SparkConf()
 
