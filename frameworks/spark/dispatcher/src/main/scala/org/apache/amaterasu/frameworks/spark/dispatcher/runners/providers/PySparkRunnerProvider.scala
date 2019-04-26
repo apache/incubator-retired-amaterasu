667bb98319cce4bce4b8a4a769f72fb246f12d91
Yaniv Rodenski <yaniv@shinto.io>
2019-04-26 14:59:00 +1000

    fixing virtual env config

--- frameworks/spark/dispatcher/src/main/scala/org/apache/amaterasu/frameworks/spark/dispatcher/runners/providers/PySparkRunnerProvider.scala
+++ frameworks/spark/dispatcher/src/main/scala/org/apache/amaterasu/frameworks/spark/dispatcher/runners/providers/PySparkRunnerProvider.scala
@@ -11,9 +11,9 @@ class PySparkRunnerProvider(val env: String, val conf: ClusterConfig) extends Py
     log.info(s"===> Cluster manager: ${conf.mode}")
     command +
       //s" $$SPARK_HOME/conf/spark-env.sh" +
-      s" && env PYSPARK_PYTHON=$getVirtualPythonPath" +
-      s" env PYSPARK_DRIVER_PYTHON=$getVirtualPythonPath" +
-        s" $$SPARK_HOME/bin/spark-submit --master yarn-cluster --files $$SPARK_HOME/conf/hive-site.xml ${actionData.getSrc}"
+     // s" && env PYSPARK_PYTHON=$getVirtualPythonPath" +
+      //s" env PYSPARK_DRIVER_PYTHON=$getVirtualPythonPath" + d
+        s" $$SPARK_HOME/bin/spark-submit --master yarn-cluster --conf spark.pyspark.virtualenv.enabled=true  --conf spark.pyspark.virtualenv.type=native--conf spark.pyspark.virtualenv.bin.path=$getVirtualPythonBin --conf spark.pyspark.python=$getVirtualPythonPath --files $$SPARK_HOME/conf/hive-site.xml ${actionData.getSrc}"
   }
 
   override def getRunnerResources: Array[String] = {
