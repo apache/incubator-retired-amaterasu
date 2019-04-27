cefc2b0060291488ad0ad6f772165066947b93e2
Nadav Har Tzvi <nadavha@apache.org>
2019-04-27 15:22:17 +0300

    duh

--- frameworks/spark/dispatcher/src/main/scala/org/apache/amaterasu/frameworks/spark/dispatcher/runners/providers/PySparkRunnerProvider.scala
+++ frameworks/spark/dispatcher/src/main/scala/org/apache/amaterasu/frameworks/spark/dispatcher/runners/providers/PySparkRunnerProvider.scala
@@ -14,7 +14,7 @@ class PySparkRunnerProvider(val env: String, val conf: ClusterConfig) extends Py
       // s" && env PYSPARK_PYTHON=$getVirtualPythonPath" +
       //s" env PYSPARK_DRIVER_PYTHON=$getVirtualPythonPath" + d
       s" && $$SPARK_HOME/bin/spark-submit --master yarn-client " +
-      s"--conf spark.pyspark.python=${conf.pythonPath}" +
+      s"--conf spark.pyspark.python=${conf.pythonPath} " +
       s"--conf spark.pyspark.driver.python=$getVirtualPythonPath " +
       s"--files $$SPARK_HOME/conf/hive-site.xml ${actionData.getSrc}"
   }
